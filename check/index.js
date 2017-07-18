31a11f5ce7fd12a2effced2722b0f1ab3fa7aa3d
ylws <474569696@qq.com>
2017-07-18 17:53:53 +0800

    mofify v

--- check/index.js
+++ check/index.js
@@ -1,6 +1,6 @@
-import {CFCMCheck} from './src/Plugins_es6_checkbox.babel.min.js'
+import {CFCMCheckbox} from './src/Plugins_es6_checkbox.babel.min.js'
 export default {
  	install: function(Vue) {
-	 Object.defineProperty(Vue.prototype, '$Check', { value: CFCMCheck });
+	 Object.defineProperty(Vue.prototype, '$Check', { value: CFCMCheckbox });
   }
 }
